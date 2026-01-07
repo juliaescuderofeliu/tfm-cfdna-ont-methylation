c1a132f284b803a09e46333c0107189f9fd7946a
juliaescuderofeliu <juliaescuderofeliu@gmail.com>
2026-01-07 16:14:31 +0100

    Update filter_canonical_windows_1Mb.R

--- scripts/01_analysis/filter_canonical_windows_1Mb.R
+++ scripts/01_analysis/filter_canonical_windows_1Mb.R
@@ -1,1 +1,13 @@
+library(dplyr)
+library(readr)
 
+df <- read_tsv("results/windows_1Mb_diff_CRC_vs_CONTROL.tsv")
+
+canon <- paste0("chr", c(1:22, "X", "Y"))
+
+df_can <- df %>% filter(chr %in% canon)
+
+write_tsv(df_can, "results/windows_1Mb_diff_CRC_vs_CONTROL_canonical.tsv")
+
+cat("DONE: results/windows_1Mb_diff_CRC_vs_CONTROL_canonical.tsv\n")
+cat("N windows:", nrow(df_can), "\n")
