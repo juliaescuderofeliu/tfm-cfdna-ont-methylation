32515f2330bf82fc33c7b78a6b83dc57b9522b7d
juliaescuderofeliu <juliaescuderofeliu@gmail.com>
2026-01-07 16:11:52 +0100

    Update merge_windows_1Mb.R

--- scripts/00_build_db/merge_windows_1Mb.R
+++ scripts/00_build_db/merge_windows_1Mb.R
@@ -1,1 +1,23 @@
+library(dplyr)
+library(readr)
+library(purrr)
+
+indir <- "results/windows_1Mb"
+files <- list.files(indir, pattern = "\\.tsv$", full.names = TRUE)
+
+message("Leyendo ", length(files), " archivos")
+
+dfs <- lapply(files, function(f) {
+  sample <- sub("\\.windows1Mb\\.cov5\\.tsv$", "", basename(f))
+  read_tsv(f, show_col_types = FALSE) %>%
+    select(chr, win_start, win_end, perc_mod) %>%
+    mutate(sample = sample)
+})
+
+all <- bind_rows(dfs)
+
+write_tsv(all, "results/windows_1Mb_all_samples_long.tsv")
+
+message("DONE: results/windows_1Mb_all_samples_long.tsv")
+
 
